a759c5c780802dd79f5c566f7aed0876c746119c
Baldmaster <sergey.archlinux@gmail.com>
2015-02-03 21:33:55 +0200

    properties list

--- jstohask/parse.hs
+++ jstohask/parse.hs
@@ -5,17 +5,17 @@ import Haste.Parsing
 import Haste.Serialize
 import Data.Either
 
-ins      = toJSStr "ins"
-outs     = toJSStr "outs"
-hash     = toJSStr "hash"
-index    = toJSStr "index"
-script   = toJSStr "script"
-buffer   = toJSStr "buffer"
-chunks   = toJSStr "chunks"
-seq      = toJSStr "sequence"
-value    = toJSStr "value"
-version  = toJSStr "version"
-locktime = toJSStr "locktime"
+ins       = toJSStr "ins"
+outs      = toJSStr "outs"
+hash      = toJSStr "hash"
+index     = toJSStr "index"
+script    = toJSStr "script"
+buffer    = toJSStr "buffer"
+chunks    = toJSStr "chunks"
+sequence' = toJSStr "sequence"
+value     = toJSStr "value"
+version   = toJSStr "version"
+locktime  = toJSStr "locktime"
 
 -- parse string to JSON then
 -- pack it's part to JS object and send back
