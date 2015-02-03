9c0322f23d22691663490f5490a1cf0d97070e7c
Baldmaster <sergey.archlinux@gmail.com>
2015-02-03 21:12:02 +0200

    get property by key and return as js object

--- jstohask/parse.hs
+++ jstohask/parse.hs
@@ -1,14 +1,18 @@
 import Haste.Foreign
 import Haste.Prim 
 import Haste.JSON
+import Haste.Parsing
+import Haste.Serialize
 import Data.Either
 
+
 -- parse string to JSON then
--- pack it to JS object and send back
-parse :: String -> IO JSAny
-parse ob = return $ toObject x
-  where Right x = decodeJSON $ toJSStr ob
-       
+-- pack it's part to JS object and send back
+parse :: JSString -> IO JSAny
+parse ob = return $ toObject b
+  where Right x = decodeJSON ob
+        a =  x ! (toJSStr "ins")  
+        b =  x ! (toJSStr "outs")
 
 main = do
   export (toJSStr "parse") parse
