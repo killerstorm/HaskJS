259be254accd3f242757486e2507398c4e398739
Baldmaster <sergey.archlinux@gmail.com>
2015-02-03 21:30:59 +0200

    properties list

--- jstohask/parse.hs
+++ jstohask/parse.hs
@@ -5,14 +5,25 @@ import Haste.Parsing
 import Haste.Serialize
 import Data.Either
 
+ins      = toJSStr "ins"
+outs     = toJSStr "outs"
+hash     = toJSStr "hash"
+index    = toJSStr "index"
+script   = toJSStr "script"
+buffer   = toJSStr "buffer"
+chunks   = toJSStr "chunks"
+sequence = toJSStr "sequence"
+value    = toJSStr "value"
+version  = toJSStr "version"
+locktime = toJSStr "locktime"
 
 -- parse string to JSON then
 -- pack it's part to JS object and send back
 parse :: JSString -> IO JSAny
-parse ob = return $ toObject b
+parse ob = return $ toObject a
   where Right x = decodeJSON ob
-        a =  x ! (toJSStr "ins")  
-        b =  x ! (toJSStr "outs")
+        a =  x ! ins  
+        b =  x ! outs
 
 main = do
   export (toJSStr "parse") parse
