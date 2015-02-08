36977b82c612d4ab8d75c1a0f83dd7b1fb40f5bf
Baldmaster <sergey.archlinux@gmail.com>
2015-02-08 20:16:26 +0200

    Tx instances + topological sort

--- jstohask/main.hs
+++ jstohask/main.hs
@@ -1,25 +1,24 @@
-{-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Map as Map
-import Debug.Trace
-
+ 
+type ColorKernel txPayload coinState = txPayload -> [coinState] -> [coinState]
+data ColorTx txPayload = ColorTx { payload :: txPayload, 
+                                   inputIndices :: [Int],
+                                   outputIndices :: [Int], 
+                                   outputCount :: Int}
+ 
+ 
 type TxId = String
-data Tx = Tx { payload       :: String, 
-               inputIndices  :: [Int],
-               outputIndices :: [Int], 
-               outputCount   :: Int,
-               txId          :: TxId,
-               inputs        :: [(TxId, Int)]-- list of input transactions IDs
-             } 
+type CoinId = (TxId, Int)
+type CoinStateMap coinState = Map.Map CoinId coinState
+data Tx txPayload = Tx { colorTx :: ColorTx txPayload, inputs :: [CoinId], txId :: TxId }
+ 
+-- applyTx :: ColorKernel txPayload coinState -> Tx txPayload -> CoinStateMap coinState -> CoinStateMap coinState
+instance Eq (Tx txPayload) where
+  a == b = txId a == txId b
 
-instance Eq Tx where
-   a == b = if txId a == txId b
-            then True
-            else False
-
-instance Show Tx where
-  show a = show  $ txId a ++ " >>"
-  
-
+instance Show (Tx txPayload) where
+  show tx = ">> " ++ txId tx
+ 
 maybeIndex :: [Maybe a] -> Int -> Maybe a
 maybeIndex list idx = if idx < length list
                       then list !! idx
@@ -27,37 +26,49 @@ maybeIndex list idx = if idx < length list
  
 reshape :: [a] -> [Int] -> Int -> [Maybe a]
 reshape list newIndices length = let imap = Map.fromList (zip newIndices list)
-                                 in map (`Map.lookup` imap) [0..length-1]
+                                 in map (\i -> Map.lookup i imap) [0..length-1]
  
-applyColorKernel :: Tx -> [Maybe Integer] -> [Maybe Integer]
-applyColorKernel   tx in_values = let selectedInValues  = map (maybeIndex in_values) (inputIndices tx)
-                                      inputsAreGood     = all (maybe False (\x -> True)) selectedInValues
-                                      outputIndicesOK   = all ((>) $ outputCount tx) (outputIndices tx)
-                                      allCorrect        = inputsAreGood && outputIndicesOK
-                                      kOutValues        = trivialColorKernel (payload tx) (map (\(Just x) -> x) selectedInValues)
+applyColorKernel :: ColorKernel txPayload coinState -> ColorTx txPayload -> [Maybe coinState] -> [Maybe coinState]
+applyColorKernel k tx in_values = let selectedInValues    = map (maybeIndex in_values) (inputIndices tx)
+                                      inputsAreGood       = all (maybe False (\x -> True)) selectedInValues
+                                      outputIndicesOK     = all ((>) (outputCount tx)) (outputIndices tx)
+                                      allCorrect          = inputsAreGood && outputIndicesOK
+                                      kOutValues          = k (payload tx) (map (\(Just x) -> x) selectedInValues)
                                   in if allCorrect
                                      then reshape kOutValues (outputIndices tx) (outputCount tx)
                                      else replicate (outputCount tx) Nothing
                                       
  
 trivialColorKernel :: String -> [Integer] -> [Integer]
-trivialColorKernel op in_values = let out_values = read op :: [Integer]
-                                  in if sum in_values == sum out_values 
+trivialColorKernel op in_values = let out_values :: [Integer]
+                                      out_values = read op
+                                  in if (sum in_values) == (sum out_values) 
                                      then out_values
                                      else []
-
-topologicalSort :: [Tx] -> [Tx]-> [Tx]                          
+                                          
+                                          
+topologicalSort :: [Tx txPayload] -> [Tx txPayload]-> [Tx txPayload]                        
 topologicalSort g tx = tsort tx []        
-                       where tsort [] r           = r
+                       where tsort [] r           = r                  -- return sorted list
                              tsort (x:xs) visited
                                | elem x visited   = tsort xs visited   -- Tx already visited
                                | otherwise        = tsort xs (x : (tsort (filter (\t -> elem (txId t) $ map fst (inputs x)) g) visited))
-                                                                                                    
-tx1 = Tx "[1]" [0] [0] 2 "1" [("7", 8)]
-tx2 = Tx "[3]" [0] [0] 0 "2" [("1", 8)]    
-tx3 = Tx "[4]" [0] [0] 0 "3" [("1", 8)] 
-tx4 = Tx "[5]" [0] [0] 0 "4" [("2", 8), ("3", 8)]
-tx5 = Tx "[6]" [0] [0] 0 "5" [("8", 8)]
-tx6 = Tx "[7]" [0] [0] 0 "6" [("3", 8)]
 
-main = print $ topologicalSort [tx2, tx3, tx1, tx4, tx5, tx6] [tx4]
+
+colortx = ColorTx "[7]" [0] [0] 0
+                                                    
+tx1 = Tx colortx  [("7", 8)]           "1"
+tx2 = Tx colortx  [("1", 8)]           "2"
+tx3 = Tx colortx  [("1", 8)]           "3"
+tx4 = Tx colortx  [("2", 8), ("3", 8)] "4"
+tx5 = Tx colortx  [("8", 8)]           "5"
+tx6 = Tx colortx  [("3", 8)]           "6"
+                                          
+ 
+                                          
+main = print $ topologicalSort [tx2, tx3, tx1, tx4, tx5, tx6] [tx4, tx2, tx3, tx6, tx1, tx5]
+
+
+--main = print (applyColorKernel trivialColorKernel tx inputs)
+--       where tx = ColorTx "[1]" [0] [0] 1
+--             inputs = [Just 1]
