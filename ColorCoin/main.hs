1977836c6c212a1781ed68ff8673e29cacdc6b17
Baldmaster <sergey.archlinux@gmail.com>
2015-03-18 22:42:34 +0200

    random number function

--- ColorCoin/main.hs
+++ ColorCoin/main.hs
@@ -7,6 +7,8 @@ import Haste.JSON as J
 --import Haste.Serialize
 import Data.Either
 import Control.Applicative
+import System.Random as SR
+
 
 import qualified Data.Map as Map
 
@@ -17,6 +19,16 @@ import TransactionGraph
 
 instance Pack JSON
 instance Unpack JSON
+instance Pack Integer
+instance Unpack Integer
+instance Pack (WrappedCS Integer)
+instance Unpack (WrappedCS Integer)
+
+newtype Seed = Seed (Int, SR.StdGen)
+
+pick :: [a] -> a
+pick xs = (xs !!) $  fst $ Haste.randomR (0, length xs - 1) (mkSeed 5)
+
 
 apply' = (applyTx (toyMuxCoinKernel
            (toyDispatchCoinKernel (Map.fromList [(0, (strictCoinKernel transferCK)),
@@ -46,11 +58,35 @@ getMuxShape payload = return $ ms
           Just x -> show $ fst x
           _      -> "Nothing"
 
-runCoinKernelOnIssue :: (String, [CoinId]) -> IO [Int]
-runCoinKernelOnIssue (x, xs) = return $ map (\x -> snd x) xs
+runCoinKernel :: (String, [(CoinId, Integer)]) -> IO [String]
+runCoinKernel (x, xs) = return $ "":[] -- $ map (\x -> (show $ fst x) ++ (show $ snd x)) $  (coins 10 xs)
+  where coins count unspent | count == 0  = unspent
+                            | otherwise   = coins (count - 1) (Map.toList
+                                                           (foldTxGraph (getGraph unspent) apply') ++ unspent)
+        
+--TODO: Random coins pick. Create transactions with random payload. Run coinKernel and save result to unspent.
+--getGraph xs   =  (Tx (getPayload p) (inputs : []) "9" 1) : []
+--  where
+--                     p      = pick xs
+--                     inputs = fst $ p
+--                     
+--getPayload xs =  "([], [0], 1) 1 " ++ (show $ (snd xs) : [])
+
+getRandomNum :: Int -> IO [Int]
+getRandomNum n = getRand n []
+  where getRand counter acc
+              | counter == 0     = return acc
+              | otherwise        = do
+                     g <- SR.newStdGen
+                     let t = head $ SR.randomRs (1, n) g
+                     --print t
+                     getRand (counter - 1) $ t  : acc
+
+
 
 
 main = do
   export (toJSStr "runCoinKernelOnGraph") runCoinKernelOnGraph
-  export (toJSStr "runCoinKernelOnIssue") runCoinKernelOnIssue
+  export (toJSStr "runCoinKernel") runCoinKernel
   export (toJSStr "getMuxShape") getMuxShape
+  export (toJSStr "getRandomNum") getRandomNum
