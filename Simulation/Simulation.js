4585366e7ed9a8b9e1aece178627939c4108a4e5
Baldmaster <sergey.archlinux@gmail.com>
2015-05-31 09:41:13 +0300

    addTx

--- Simulation/Simulation.js
+++ Simulation/Simulation.js
@@ -1,43 +1,45 @@
+var _ = require('lodash')
+
 function Simulation() {
-  this.transactions = [];
-  this.wallets = {};
-  this.coins = [];
+    this.transactions = [];
+    this.wallets = {};
+    this.coins = [];
 }
  
 Simulation.prototype.wallet = function (name) {
-  this.wallets[name] = new Wallet(this, name);
-  return this.wallets[name];
+    this.wallets[name] = new Wallet(this, name);
+    return this.wallets[name];
 }
  
 Simulation.prototype.addTx = function (tx) {
-    
+    this.transactions.push(tx);
 }
 
 Simulation.prototype.getUnspentCoins(addr) {
-    //TODO
+    
 }
 
 function Wallet(simulation, name) {
-  this.simulation = simulation;
-  this.privkey = name; 
-  this.pubkey =  name; 
-  this.address = name; 
+    this.simulation = simulation;
+    this.privkey = name; 
+    this.pubkey =  name; 
+    this.address = name; 
 }
  
 Wallet.prototype.issueCoin = function (kernel, value) {
-  var tx = this.simulation.composeIssueTx(kernel, value, this.getUnspentCoins());
-  var signedTx = this.signTx(tx);
-  this.simulation.addTx(signedTx);
+    var tx = this.simulation.composeIssueTx(kernel, value, this.getUnspentCoins());
+    var signedTx = this.signTx(tx);
+    this.simulation.addTx(signedTx);
 }
  
 Wallet.prototype.getUnspentCoins = function () {
-  return this.simulation.getUnspentCoins(this.address);
+    return this.simulation.getUnspentCoins(this.address);
 }
  
 Wallet.prototype.signTx = function (tx) {
-  // TODO
-  // nobody's going to check if we have signed it so unsigned works too
-  return tx;  
+    // TODO
+    // nobody's going to check if we have signed it so unsigned works too
+    return tx;  
 }
 
 Wallet.prototype.getAddress() {
