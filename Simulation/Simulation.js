e8ca52b720eea54ac6b8c56975801ea5116d5eb8
Baldmaster <sergey.archlinux@gmail.com>
2015-05-31 08:56:38 +0300

    Simulation.js update

--- Simulation/Simulation.js
+++ Simulation/Simulation.js
@@ -17,4 +17,31 @@ Simulation.prototype.getUnspentCoins(addr) {
     //TODO
 }
 
+function Wallet(simulation, name) {
+  this.simulation = simulation;
+  this.privkey = name; 
+  this.pubkey =  name; 
+  this.address = name; 
+}
+ 
+Wallet.prototype.issueCoin = function (kernel, value) {
+  var tx = this.simulation.composeIssueTx(kernel, value, this.getUnspentCoins());
+  var signedTx = this.signTx(tx);
+  this.simulation.addTx(signedTx);
+}
+ 
+Wallet.prototype.getUnspentCoins = function () {
+  return this.simulation.getUnspentCoins(this.address);
+}
+ 
+Wallet.prototype.signTx = function (tx) {
+  // TODO
+  // nobody's going to check if we have signed it so unsigned works too
+  return tx;  
+}
+
+Wallet.prototype.getAddress() {
+    return this.address;
+}
+
 module.exports = Simulation
