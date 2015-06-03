b17851c11609c461e19d055c064cbf0a22f49a83
Baldmaster <sergey.archlinux@gmail.com>
2015-06-03 21:28:49 +0300

    kernel.js update

--- Simulation/Kernel.js
+++ Simulation/Kernel.js
@@ -1,13 +1,26 @@
-function Kernel() {
-    //TODO
+var maketx = require('../Maketx/src/maketx.js')
+var coinkernel = require('../ColorCoin/src/ckernel.js')
+var h = require('../ColorCoin/main.js').getHaste()
 
+function Kernel() {
+    
 }
 
 function Color() {
+    throw new Error ("Color not implemented");
     //TODO
 }
 
 
 function ColorValue() {
+    throw new Error ("ColorValue not implemented");
     //TODO
+
+}
+
+
+module.exports = {
+    Kernel     : Kernel,
+    Color      : Color,
+    ColorValue : ColorValue
 }
