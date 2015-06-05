ec2b4182c4b7c3b1687a88c6e0214e136d6c976d
Baldmaster <sergey.archlinux@gmail.com>
2015-06-05 15:45:11 +0300

    Kernel.js, (run and composeTx functions)

--- Simulation/Kernel.js
+++ Simulation/Kernel.js
@@ -1,11 +1,32 @@
-var maketx = require('../Maketx/src/maketx.js')
-var coinkernel = require('../ColorCoin/src/ckernel.js')
-var h = require('../ColorCoin/main.js').getHaste()
+var composetx = require('../ColorCoin/src/composeTx.js');
+var ckernel = require('../ColorCoin/src/ckernel.js');
+var haste = require('../ColorCoin/main.js').getHaste();
 
 function Kernel() {
     
 }
 
+Kernel.prototype.run = function(tx) {
+    var _tx = ckernel.createTx(tx);
+    var coins = haste.runKernel(_tx);
+    return coins;
+}
+
+Kernel.prototype.composeIssueTx = function (value, targets) {
+    var tx = composetx.composeColoredIssueTx (value, targets);
+    return tx;
+}
+
+Kernel.prototype.composeSendTx = function (unspent, targets, changeAddress) {
+    var tx = composetx.composeColoredSendTx (unspent, targets, changeAddress);
+    return tx;
+}
+
+Kernel.prototype.composeBitcoinTx = function (tx, unspent) {
+    var tx = composetx.composeBitcoinTx (tx, unspent);
+    return tx;
+}
+        
 function Color() {
     throw new Error ("Color not implemented");
     //TODO
