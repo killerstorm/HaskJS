950f4bab4cbcbcc6e2be7eb14e649e45f610650f
Baldmaster <sergey.archlinux@gmail.com>
2015-06-05 19:55:43 +0300

    test.js update

--- Simulation/test.js
+++ Simulation/test.js
@@ -1,9 +1,14 @@
 var Simulation = require('./simulation.js')
 var kernel     = require('./kernel.js')
+var bitcoin    = require('bitcoinjs-lib')
 
 
+const coinbaseTxId = '0000000000000000000000000000000000000000000000000000000000000000';
+const coinbaseOutIndex = 0xFFFFFFFF;
 
 var sim = new Simulation();
+
+
 
 var issuer = sim.wallet('issuer');
 var alice  = sim.wallet('alice');
@@ -13,8 +18,8 @@ console.log("Issuer's address:\t" + issuer.getAddress());
 console.log("Alice's address:\t" +  alice.getAddress());
 console.log("Bob's address:\t\t" +  bob.getAddress());
 
-
-
+var coinbaseTx = new bitcoin.Transaction();
+coinbaseTx.addInput(coinbaseTxId, coinbaseOutIndex);
 
 
 
