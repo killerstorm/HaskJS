9ec65d1eb95626e78e495f907298ea1b15d08059
Baldmaster <sergey.archlinux@gmail.com>
2015-06-05 19:33:39 +0300

    test update

--- Simulation/test.js
+++ Simulation/test.js
@@ -1,4 +1,24 @@
-var simulation = require('./Simulation.js')
-var kernel     = require('./Kernel.js')
+var Simulation = require('./simulation.js')
+var kernel     = require('./kernel.js')
+
+
+
+var sim = new Simulation();
+
+var issuer = sim.wallet('issuer');
+var alice  = sim.wallet('alice');
+var bob    = sim.wallet('bob');
+
+console.log("Issuer's address:\t" + issuer.getAddress());
+console.log("Alice's address:\t" +  alice.getAddress());
+console.log("Bob's address:\t\t" +  bob.getAddress());
+
+
+
+
+
+
+
+
 
 
