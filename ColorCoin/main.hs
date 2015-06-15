e01f281df4e6ac887ded099e1eb2a29e04f9f535
Baldmaster <sergey.archlinux@gmail.com>
2015-06-15 19:56:21 +0300

    coinstate -> value

--- ColorCoin/main.hs
+++ ColorCoin/main.hs
@@ -38,7 +38,7 @@ packToJSON ((txid, index), cs) = toJSStr $
   "{" ++
   "\"txid\" : \""  ++ txid       ++ "\", " ++
   "\"index\" : " ++ show index ++ ", " ++
-  "\"coinstate\" : \""    ++ coinstate  ++ "\"}"
+  "\"value\" : \""    ++ coinstate  ++ "\"}"
   where coinstate = case cs of
           JustCS x  -> show x
           MissingCS -> "M"
