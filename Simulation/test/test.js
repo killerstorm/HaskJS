2004987b0406894b23a519fae208cef961dd1c9b
Baldmaster <sergey.archlinux@gmail.com>
2015-06-16 19:05:12 +0300

    test update

--- Simulation/test/test.js
+++ Simulation/test/test.js
@@ -41,6 +41,7 @@ describe("Simulation", function() {
             var uncoloredBalance = uncolored.getBalance();
             expect(uncoloredBalance).to.equal(2500000000);
         });
+        
         describe("Issue coin", function() {
             it("Alice issues 10 bitcoins coin", function() {
                 alice.issueCoin(1000000000);
@@ -50,6 +51,7 @@ describe("Simulation", function() {
                 bob.issueCoin(2000000000);
             });
         });
+        
         describe("Get balance", function() {
             it("Alice's balance, expected to equal 1000000000 satoshi", function() {
                 var balance = alice.getBalance();
@@ -60,6 +62,25 @@ describe("Simulation", function() {
                 var balance = bob.getBalance();
                 expect(balance).to.equal(2000000000);
             });
+        });
+
+        describe("Send", function() {
+            it("Alice sends 1000000 satoshi to Bob", function() {
+                alice.send(1000000, bob);
+            });
+
+            it ("Alice's balance = 999000000", function() {
+                expect(alice.getBalance()).to.equal(999000000);
+            })
+
+            it("Bob sends 1000000000 satoshi to Alice", function() {
+                bob.send(1000000000, alice);
+            });
+
+            it ("Bob's balance = 1001000000", function() {
+                expect(bob.getBalance()).to.equal(1001000000);
+            });
+
         });
         
     });
