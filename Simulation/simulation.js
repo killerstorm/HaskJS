6754a99374fbb51c6c0a0f1dbe3d6ce241ba5e9f
Baldmaster <sergey.archlinux@gmail.com>
2015-06-22 13:26:09 +0300

    simulation.js 2 spaces indentation

--- Simulation/simulation.js
+++ Simulation/simulation.js
@@ -28,124 +28,157 @@ const x80 = new Buffer('80', 'hex');
  */
 const x01 = new Buffer('01', 'hex');
 
+/**
+ * Simulation
+ * @interface
+*/
 function Simulation(name) {
-    this.name = name || 'test';
-    this.kernel = new kernel.Kernel(this);
-    this.transactions = [];
-    this.wallets = {};
-    this.coins = [];
-    this.wallet('uncolored');
-    this.init();
+  this.name = name || 'test';
+  this.kernel = new kernel.Kernel(this);
+  this.transactions = [];
+  this.wallets = {};
+  this.coins = [];
+  this.wallet('uncolored');
+  this.init();
 }
+
 
 Simulation.prototype.init = function() {
-    var coinbaseTx = new bitcoin.Transaction();
-    coinbaseTx.addInput(coinbaseTxId, coinbaseOutIndex);
-    coinbaseTx.addOutput(this.wallets['uncolored'].getAddress(), 2500000000);
-    coinbaseTx.addOutput(bitcoin.scripts.nullDataOutput(
-        new Buffer ('([] [0] 1) 1 [2500000000]')), 0);
-    
-    this.addTx(coinbaseTx);
-    this.addCoins([{"txid" : coinbaseTx.getId(), "index" : 0, "coinstate" : "2500000000", "value" : 2500000000}]);
-    
+  var coinbaseTx = new bitcoin.Transaction();
+  coinbaseTx.addInput(coinbaseTxId, coinbaseOutIndex);
+  coinbaseTx.addOutput(this.wallets['uncolored'].getAddress(), 2500000000);
+  coinbaseTx.addOutput(bitcoin.scripts.nullDataOutput(
+      new Buffer ('([] [0] 1) 1 [2500000000]')), 0);
+  
+  this.addTx(coinbaseTx);
+  this.addCoins(
+    [
+      {
+        "txid" : coinbaseTx.getId(),
+        "index" : 0,
+        "coinstate" : "2500000000",
+        "value" : 2500000000
+      }
+    ]
+  );
+  
 }
 
+
 Simulation.prototype.wallet = function (name) {
-    this.wallets[name] = new Wallet(this, name);
-    return this.wallets[name];  
+  this.wallets[name] = new Wallet(this, name);
+  return this.wallets[name];  
 }
  
 Simulation.prototype.addTx = function (tx) {
-    return this.transactions.push(tx);
+  return this.transactions.push(tx);
 }
 
-Simulation.prototype.addCoins = function(coins) {
-    this.coins = this.coins.concat(coins);
+Simulation.prototype.addCoins = function (coins) {
+  this.coins = this.coins.concat(coins);
 }
 
 Simulation.prototype.getUnspentCoins = function (addr) {
-    var unspent = [];
-    var sim = this;
+  var unspent = [];
+  var sim = this;
     
-    _.map(sim.transactions, function(tx) {
-        var index = 0;
-        _.each(tx.outs, function(out) {
-            if (out.script.chunks.length != 2 &&
-                bitcoin.Address.fromOutputScript(out.script).toString() == addr) {
-                _.find(sim.coins, function (c) {
-                    return (
-                        c.txid == tx.getId() &&
-                        c.index == index     &&
-                        unspent.push(c)
-                    );
-                });
-            }
-            index++;
-        });
-    });
-    this.coins = _.difference(this.coins, unspent);
-    return unspent;                   
+  _.map(sim.transactions, function (tx) {
+    var index = 0;
+      _.each (tx.outs, function (out) {
+        if (out.script.chunks.length != 2 &&
+            bitcoin.Address.fromOutputScript(out.script).toString() == addr) {
+          _.find(sim.coins, function (c) {
+            return (
+              c.txid  == tx.getId() &&
+              c.index == index      &&
+              unspent.push(c)
+            );
+          });
+        }
+        index++;
+      });
+  });
+  
+  this.coins = _.difference(this.coins, unspent);
+  return unspent;                   
 }
 
 function Wallet(simulation, name) {
-    this.name = name; 
-    this.simulation = simulation;
-    this.key     = bitcoin.crypto.sha256(name);
-    this.wif     = this.getWIF();
-    this.privkey = bitcoin.ECKey.fromWIF(this.wif)
-    this.pubkey  = this.privkey.pub;
-    this.address = this.pubkey.getAddress().toString();
-    this.coins = [];
+  this.name       = name;
+  this.simulation = simulation;
+  this.key        = bitcoin.crypto.sha256(name);
+  this.wif        = this.getWIF();
+  this.privkey    = bitcoin.ECKey.fromWIF(this.wif)
+  this.pubkey     = this.privkey.pub;
+  this.address    = this.pubkey.getAddress().toString();
+  this.coins      = [];
 }
+
 
 Wallet.prototype.getWIF = function() {
-    var extkey = new Buffer.concat([new Buffer('80', 'hex'), this.key]);
-    var checksum = (sha256(sha256(extkey))).slice(0, 4);
-    var wif      = base58.encode(new Buffer.concat([extkey, checksum]));
-    return wif;
+  var extkey   = new Buffer.concat([x80, this.key, x01]);
+  var checksum = (sha256(sha256(extkey))).slice(0, 4);
+  var wif      = base58.encode(new Buffer.concat([extkey, checksum]));
+  return wif;
 }
     
- 
 Wallet.prototype.issueCoin = function (value) {
-    var coloredTx = this.simulation.kernel.composeIssueTx([{'address' : this.getAddress(), 'value' : value}]);
-    //txio => {tx, inputs, outValues}
-    var txio = this.simulation.kernel.composeBitcoinTx (coloredTx, this.simulation.wallets['uncolored']);
-    var tx = this.signTx(txio.tx);
-    this.simulation.addTx(tx);
-    this.simulation.addCoins(this.simulation.kernel.runKernel(tx, txio.inputs, txio.outValues));
+  var coloredTx = this.simulation.kernel.composeIssueTx(
+    [{'address' : this.getAddress(), 'value' : value}]
+  );
+  
+  var txio = this.simulation.kernel.composeBitcoinTx (
+    coloredTx, this.simulation.wallets['uncolored']);
+  
+  var tx = this.signTx(txio.tx);
+  
+  this.simulation.addTx(tx);
+  
+  this.simulation.addCoins(
+    this.simulation.kernel.runKernel(tx, txio.inputs, txio.outValues)
+  );
 }
 
-
 Wallet.prototype.send = function (value, target) {
-    var coloredTx = this.simulation.kernel.composeSendTx(this,
-                                                  [{'address' : target.getAddress(), 'value' : value}],
-                                                         this.getAddress());
-    //txio => {tx, inputs, outValues}
-    var txio = this.simulation.kernel.composeBitcoinTx (coloredTx, this.simulation.wallets['uncolored']);
-    var tx = this.signTx(txio.tx);
-    this.simulation.addTx(tx);
-    this.simulation.addCoins(this.simulation.kernel.runKernel(tx, txio.inputs, txio.outValues));
+  var coloredTx = this.simulation.kernel.composeSendTx(
+    this,
+    [{'address' : target.getAddress(),'value' : value}],
+    this.getAddress()
+  );
+  
+  //txio => {tx, inputs, outValues}
+  var txio = this.simulation.kernel.composeBitcoinTx (
+    coloredTx,
+    this.simulation.wallets['uncolored']
+  );
+  
+  var tx = this.signTx(txio.tx);
+  this.simulation.addTx(tx);
+  
+  this.simulation.addCoins(
+    this.simulation.kernel.runKernel(tx, txio.inputs, txio.outValues)
+  );
 }
 
 Wallet.prototype.getUnspentCoins = function () {
-    this.coins = this.coins.concat(this.simulation.getUnspentCoins(this.address));
-    return this.coins;
+  this.coins = this.coins.concat(this.simulation.getUnspentCoins(this.address));
+  return this.coins;
 }
  
 Wallet.prototype.signTx = function (tx) {
-    var txb = new bitcoin.TransactionBuilder.fromTransaction(tx);
-    for (var i = 0; i < txb.tx.ins.length; txb.sign(i, this.privkey), i++);
-    tx = txb.build();
-    return tx;
+  var txb = new bitcoin.TransactionBuilder.fromTransaction(tx);
+  for (var i = 0; i < txb.tx.ins.length; txb.sign(i, this.privkey), i++);
+  tx = txb.build();
+  return tx;
 }
 
 Wallet.prototype.getBalance = function() {
-    this.getUnspentCoins();
-    return _.sum(this.coins, "value");
+  this.getUnspentCoins();
+  return _.sum(this.coins, "value");
 }
 
 Wallet.prototype.getAddress = function() {
-    return this.address;
+  return this.address;
 }
 
 module.exports = Simulation
