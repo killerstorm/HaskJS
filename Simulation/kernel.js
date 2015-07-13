f676bce140a6b5b1dc9924f95562ee850295642a
Baldmaster <sergey.archlinux@gmail.com>
2015-07-13 23:00:28 +0300

    runCoinKernelOnGraph created

--- Simulation/kernel.js
+++ Simulation/kernel.js
@@ -1,8 +1,12 @@
 var composetx = require('./composeTx.js');
-var kerneltx  = require('../src/kerneltx.js');
-var haste     = require('../main.js').getHaste();
+var kerneltx  = require('../lib/kerneltx.js');
+var haste     = require('../lib/Haste.js').getHaste();
 var _         = require('lodash');
 
+
+var createKernelTx = kerneltx.createKernelTx
+var runKernel = haste.runKernel
+var runCoinKernelOnGraph = haste.runCoinKernelOnGraph
 
 function Kernel (simulation) {
   this.simulation = simulation;
@@ -10,12 +14,28 @@ function Kernel (simulation) {
 
 Kernel.prototype.runKernel = function (tx) {
 
-  var optx = kerneltx.createKernelTx (tx) 
+  var optx = createKernelTx (tx) 
   
-  var coins = _.map(haste.runKernel(JSON.stringify(optx)), JSON.parse);
+  var coins = _.map(runKernel(JSON.stringify(optx)), JSON.parse)
 
   return coins;
 }
+
+Kernel.prototype.runKernelOnGraph = function (tx) {
+  var transactions = _.chain(this.simulation.transactions)
+                     .map(createKernelTx)
+                     .map(JSON.stringify)
+                     .value()
+
+  console.log(transactions)
+  var optx = JSON.stringify(createKernelTx(tx))
+  
+  var coins = _.map(runCoinKernelOnGraph(transactions, optx), JSON.parse)
+
+  return coins
+}
+
+
         
 function Color () {
   throw new Error ("Color not implemented");
