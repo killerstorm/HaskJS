020f33fbbf0adee2f79dbd2fd1983dab9e57b8b6
Baldmaster <sergey.archlinux@gmail.com>
2015-07-16 12:34:12 +0300

    CoinKernel refactoring

--- Src/CoinKernel.hs
+++ Src/CoinKernel.hs
@@ -25,26 +25,24 @@ invalidCS _ = False
  
 remapInputs :: [Int] -> [WrappedCS a] -> [WrappedCS a]
 remapInputs inputIndices inputs = map getinput inputIndices
-  where getinput i = if (i < length inputs)
+  where getinput i = if i < length inputs
                      then inputs !! i
                      else InvalidCS
  
 remapOutputs outputIndices outputCount outputs = 
-  let idxmap = Map.fromList (zip outputIndices (if (length outputs) == (length outputIndices)
+  let idxmap = Map.fromList (zip outputIndices (if length outputs == length outputIndices
                                                then outputs
-                                               else (replicate (length outputIndices) InvalidCS)))
-  in map (\i -> case Map.lookup i idxmap of
-             Just output -> output
-             Nothing -> NullCS) [0..outputCount - 1]
+                                               else replicate (length outputIndices) InvalidCS))
+  in map (\i ->  fromMaybe NullCS (Map.lookup i idxmap)) [0..outputCount - 1]
  
 strictTransactor :: Transactor a -> WCSTransactor a
-strictTransactor txop inputs = if (all properCS inputs)
+strictTransactor txop inputs = if all properCS inputs
                                then map JustCS $ txop (map (\(JustCS cs) -> cs) inputs)
                                else []
  
  
 strictCoinKernel :: CoinKernel tx a -> WCSCoinKernel tx a
-strictCoinKernel innerKernel = \tx -> strictTransactor (innerKernel tx)
+strictCoinKernel innerKernel tx = strictTransactor . innerKernel tx
  
  
 strictMux :: WCSTransactor a -> MuxShape -> WCSTransactor a
@@ -56,18 +54,18 @@ strictMux txop (inputIndices, outputIndices, outputCount) inputs =
   in remapOutputs outputIndices outputCount outputs
  
 parseMuxShape :: String -> Maybe (MuxShape, String)
-parseMuxShape s = case (reads s) of
+parseMuxShape s = case reads s of
   [res] -> Just res
   _ -> Nothing
      
 toyMuxCoinKernel :: WCSCoinKernel String cs -> WCSCoinKernel String cs
-toyMuxCoinKernel innerKernel = \str-> case parseMuxShape str of
+toyMuxCoinKernel innerKernel str = case parseMuxShape str of
   Just (muxShape, rest) -> strictMux (innerKernel rest) muxShape
   Nothing -> const []
  
  
 parseId :: String -> Maybe (Int, String)
-parseId s = case (reads s) of
+parseId s = case reads s of
   [res] -> Just res
   _     -> Nothing
   
@@ -81,7 +79,7 @@ toyDispatchCoinKernel table str = case parseId str of
 transferCK :: String -> [Integer] -> [Integer]
 transferCK op in_values =  let out_values :: [Integer]
                                out_values = read op
-                           in if (sum in_values) == (sum out_values) 
+                           in if sum in_values == sum out_values
                                     then out_values
                                     else []                            
 
