5bf66bc24f628e50c5a3af5c23a8ce92ace5209e
Baldmaster <sergey.archlinux@gmail.com>
2015-07-19 13:54:18 +0300

    Kernel update, select kernel by name

--- Simulation/kernel.js
+++ Simulation/kernel.js
@@ -1,45 +1,54 @@
-var composetx  = require('./composeTx.js');
-var kerneltx   = require('../lib/kerneltx.js');
-var haste      = require('../lib/Haste.js').getHaste();
-var _          = require('lodash');
+var composetx  = require('./composeTx.js')
+var kerneltx   = require('../lib/kerneltx.js')
+var haste      = require('../lib/Haste.js').getHaste()
+var _          = require('lodash')
 
 var createKernelTx = kerneltx.createKernelTx
-var runKernel = haste.runKernel
-var runCoinKernelOnGraph = haste.runCoinKernelOnGraph
+var _runKernel = haste.runKernel
+var _runCoinKernelOnGraph = haste.runCoinKernelOnGraph
 
 /**
  * Kernel.
  * @constructor
  */
-function Kernel (simulation) {
-  this.simulation = simulation;
+function Kernel (simulation, kernelName) {
+  switch (kernelName) {
+    case 'toy' :
+      this.runKernel = runKernel
+      this.runCoinKernelOnGraph = runCoinKernelOnGraph
+      break
+    default:
+      throw new Error("Kernel does not exist!")
+  }
+  this.simulation = simulation
 }
+
 
 /**
  * run kernel
  * @param {string} tx
  */
-Kernel.prototype.runKernel = function (tx) {
+function runKernel (tx) {
 
   var optx = createKernelTx (tx) 
-  var coins = _.map(runKernel(JSON.stringify(optx)), JSON.parse)
+  var coins = _.map(_runKernel(JSON.stringify(optx)), JSON.parse)
 
-  return coins;
+  return coins
 }
 
 /**
  * run kernel on graph
  * @param {string} tx
  */
-Kernel.prototype.runKernelOnGraph = function (tx) {
+function runCoinKernelOnGraph (tx) {
   var transactions = _.chain(this.simulation.transactions)
                      .map(createKernelTx)
                      .map(JSON.stringify)
                      .value()
-
+  
   var optx = JSON.stringify(createKernelTx(tx))
   
-  var coins = _.map(runCoinKernelOnGraph(transactions, optx), JSON.parse)
+  var coins = _.map(_runCoinKernelOnGraph(transactions, optx), JSON.parse)
 
   return coins
 }
